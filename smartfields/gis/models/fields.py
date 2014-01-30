ede0ce4d742a2494f627d6464030c708da6e4643
Alexey Kuleshevich <lehins@yandex.ru>
2014-01-30 01:44:14 -0700

    gis converter now has proper implementation

--- smartfields/gis/models/fields.py
+++ smartfields/gis/models/fields.py
@@ -1,6 +1,6 @@
 from django.contrib.gis.geos.geometry import GEOSGeometry
 from django.conf import settings
-from django.core.files.base import File
+from django.core.files.base import File, ContentFile
 from django.db.models.fields.files import FieldFile
 
 from smartfields.models import FileField
@@ -30,22 +30,29 @@ class GeoFieldFile(FieldFile):
             geometry = self.field.geometry
         if not isinstance(geometry, GEOSGeometry):
             raise TypeError("'geometry' has to be either a GEOSGeometry type or"
-                            " a string name of a non-empty GeometryField. %s" % type(geometry)) 
+                            " a string name of a non-empty GeometryField. %s" % 
+                            type(geometry))
         self.name = self.field.generate_filename(
             self.instance, "%s.%s" % (name, self.field.format.lower()))
         file_path = self.storage.path(self.name)
-        converter = self.converter_class(geometry)
-        file = converter.convert(file_path, format=self.field.format, **kwargs)
-        self.file = File(file)
+        converter = self.converter_class(geometry, encoder=self.field.encoder)
+        content = converter.convert(
+            file_path, format=self.field.format, instance=self.instance, **kwargs)
+        if isinstance(content, file):
+            self.file = File(file)
+        elif content is not None:
+            self.save(self.name, ContentFile(content), save=False)
         self._commited = True
         setattr(self.instance, self.field.name, self.name)
+
 
 
 class GeoFileField(FileField):
     attr_class = GeoFieldFile
     
-    def __init__(self, geometry=None, format='KML', **kwargs):
+    def __init__(self, geometry=None, format='KML', encoder=None, **kwargs):
         self.geometry = geometry
         self.format = format
+        self.encoder = encoder
         super(GeoFileField, self).__init__(**kwargs)
             
--- smartfields/gis/utils.py
+++ smartfields/gis/utils.py
@@ -7,11 +7,15 @@ from django.utils.six import StringIO
 from smartfields.utils import create_dirs
 
 class KMLEncoder(object):
-    def __init__(self, geometry, geo_processor=None):
+    def __init__(self, geometry, path=None, geo_processor=None, 
+                 kml_processor=None, pretty=False, **kwargs):
         self.geometry = geometry
+        self.path = path
         self._kml = simplekml.Kml()
         if callable(geo_processor):
             self._processor = geo_processor
+        self._kml_processor = kml_processor
+        self._pretty = pretty
         
     def _processor(self, obj):
         pass
@@ -33,107 +37,34 @@ class KMLEncoder(object):
             obj.innerboundaryis = g[1:]
         elif isinstance(g, GeometryCollection):
             obj = kml.newmultigeometry()
-            for mg in g:
-                self._encode(obj, mg)
-        if obj:
-            self._processor(obj)
+            for sg in g:
+                self._encode(obj, sg)
+        self._processor(obj)
+        return kml
+
+    def get_kml(self):
+        kml = self._encode(self._kml, self.geometry)
+        if callable(self._kml_processor):
+            self._kml_processor(kml)
         return kml
 
     @property
     def kml(self):
-        return self._encode(self._kml, self.geometry)
-
-class GMapsJSEncoder(object):
-    def __init__(self, geometry):
-        self.geometry = geometry
-        self.b = StringIO()
-
-    def writeLatLng(self, p):
-        self.b.write("new l(")
-        self.b.write(p[1])
-        self.b.write(",")
-        self.b.write(p[0])
-        self.b.write(")")
-
-    def writePolyline(self, polyline):
-        self.b.write("processors.polyline(new pl({path:[")
-        for p in polyline:
-            self.writeLatLng(p)
-            self.b.write(",")
-        self.b.seek(-1,1)
-        self.b.write("]}))")
-
-    def writePolygon(self, polygon):
-        self.b.write("processors.polygon(new pg({paths:[")
-        for ls in polygon:
-            self.b.write("[")
-            for p in ls[:-1]: # we don't need to include last point
-                self.writeLatLng(p)
-                self.b.write(",")
-            self.b.seek(-1,1)
-            self.b.write("],")
-        self.b.seek(-1,1)
-        self.b.write("]}))")
-
-    def writeGeometry(self, g):
-        if isinstance(g, Point):
-            self.writePoint(p)
-        elif isinstance(g, LineString):
-            self.writePolyline(g)
-        elif isinstance(g, LinearRing):
-            self.writePolygon([g])
-        elif isinstance(g, Polygon):
-            self.writePolygon(g)
-        elif isinstance(g, GeometryCollection):
-            self.b.write("[")
-            for subg in g:
-                self.writeGeometry(subg)
-                self.b.write(",")
-            self.b.seek(-1, 1)
-            self.b.write("]")
+        self.get_kml().save(self.path, format=self._pretty)
 
     @property
-    def js(self):
-        self.b.write(
-            "function construct(processors){"
-            "var id=function(o){return o;}, l=google.maps.LatLng;"
-            "var pl=google.maps.Polyline, pg=google.maps.Polygon;"
-            "processors = processors || {};"
-            "processors['polyline']=processors['polyline'] || id;"
-            "processors['polygon']=processors['polygon'] || id;return ")
-        self.writeGeometry(self.geometry)
-        self.b.write(";}")
-        value = self.b.getvalue()
-        self.b.close()
-        return value
-
+    def kmz(self):
+        self.get_kml().savekmz(self.path, format=self._pretty)
 
 class GeoConverter(object):
 
-    def __init__(self, geometry):
+    def __init__(self, geometry, encoder=None):
         self.geometry = geometry
+        self.encoder = encoder or KMLEncoder
 
     def convert(self, path, format, **kwargs):
         format = format.lower()
-        if format in ['kml', 'kmz']:
-            geo_processor = kwargs.get('geo_processor', None)
-            kml_processor = kwargs.get('kml_processor', None)
-            encoder = KMLEncoder(self.geometry, geo_processor=geo_processor)
-            kml = encoder.kml
-            if callable(kml_processor):
-                kml = kml_processor(kml)
-            pretty = kwargs.get('pretty', False)
-            create_dirs(path)
-            if format == 'kml':
-                kml.save(path, format=pretty)
-            elif format == 'kmz':
-                kml.savekmz(path, format=pretty)
-            return open(path)
-        elif format == "js":
-            encoder = GMapsJSEncoder(self.geometry)
-            create_dirs(path)
-            file = open(path, 'w')
-            file.write(encoder.js)
-            return file
-        raise AttributeError(u"Unknown format: %s" % format)
+        encoder = self.encoder(
+            self.geometry, path=path, **kwargs)
+        return getattr(encoder, format)
 
