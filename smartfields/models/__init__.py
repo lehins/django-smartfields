3e5382ccd773785cc9e940a0539fe93fb7872db0
Alexey Kuleshevich <lehins@yandex.ru>
2014-05-13 23:12:10 -0600

    Added ModelMixin removed base.py

--- smartfields/models/__init__.py
+++ smartfields/models/__init__.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from smartfields.models.base import Model
+from django.db import models
 from smartfields.models.fields import *
 from smartfields.models.fields.files import *
 from smartfields.models.fields.managers import *
@@ -12,3 +12,47 @@ if 'south' in settings.INSTALLED_APPS:
                                  '^smartfields\.models\.fields\.files\.VideoField',
                                  '^smartfields\.models\.fields\.SlugField',
                                  '^smartfields\.models\.fields\.HTMLField'])
+
+
+class ModelMixin(object):
+
+    smartfields_managers = None
+
+    def __init__(self, *args, **kwargs):
+        super(ModelMixin, self).__init__(*args, **kwargs)
+        self.smartfields_handle()
+
+
+    def save(self, *args, **kwargs):
+        self.smartfields_handle()
+        super(ModelMixin, self).save(*args, **kwargs)
+
+
+    def delete(self, *args, **kwargs):
+        if self.smartfields_managers is not None:
+            for manager in self.smartfields_managers:
+                delete_handle = getattr(manager.field, 'delete', None)
+                if callable(delete_handle):
+                    delete_handle(self)
+        return super(ModelMixin, self).delete(*args, **kwargs)
+                    
+
+
+    def smartfields_handle(self):
+        if self.smartfields_managers is not None:
+            for manager in self.smartfields_managers:
+                manager.handle(self)
+
+
+    def smartfield_status(self, field_name):
+        """A way to find out a status a filed."""
+        field = self._meta.get_field(field_name)
+        if hasattr(field, 'get_status'):
+            return field.get_status(self)
+
+
+
+class Model(ModelMixin, models.Model):
+
+    class Meta:
+        abstract = True
