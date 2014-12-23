7b7ccca81b4df93d4fb9ea37178a6e2d28bfda57
Alexey Kuleshevich <lehins@yandex.ru>
2014-12-23 13:16:15 -0700

    removed unnecessary utils

--- smartfields/utils.py
+++ smartfields/utils.py
@@ -172,25 +172,3 @@ class AsynchronousFileReader(threading.Thread):
     def eof(self):
         '''Check whether there is no more content to expect.'''
         return not self.is_alive() and self._queue.empty()
-
-
-
-def from_string_import(string):
-    """
-    Returns the attribute from a module, specified by a string.
-    """
-    module, attrib = string.rsplit('.', 1)
-    return getattr(importlib.import_module(module), attrib)
-
-
-def create_dirs(full_path):
-    directory = os.path.dirname(full_path)
-    if not os.path.exists(directory):
-        try:
-            os.makedirs(directory)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-    if not os.path.isdir(directory):
-        raise IOError("%s exists and is not a directory." % directory)
-
