389e23b696b2ba66cbf694b998cf69b86c2e79b7
Alexey Kuleshevich <lehins@yandex.ru>
2015-01-31 14:06:58 -0700

    trying to make 1.8a1 to pass tests

--- tests/models.py
+++ tests/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.core.files.base import ContentFile
 from django.utils.text import slugify
 
